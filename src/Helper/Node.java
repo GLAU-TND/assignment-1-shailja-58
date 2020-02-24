4963f2a245ab4bcf5cccb3e0f07c625abef1bbc5
shailja tripathi <shailja.tripathi_cs18@gla.ac.in>
2020-02-24 16:48:48 +0530

    getter for data

---
+++ src/Helper/Node.java
@@ -0,0 +1,14 @@
+
+package Helper;
+
+                            //Generic Node class
+public class Node<T> {
+    private T data;         //Data of node
+    private Node<T> next;   //Self referential data
+
+                            //Getter for data
+    public T getData() {
+        return data;
+    }
+
+}
