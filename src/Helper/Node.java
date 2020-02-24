6affd5b535c419ab82a71325784a9ef2a2a0411d
shailja tripathi <shailja.tripathi_cs18@gla.ac.in>
2020-02-24 16:57:58 +0530

    getter setter for self referential data

--- src/Helper/Node.java
+++ src/Helper/Node.java
@@ -15,3 +15,12 @@ public class Node<T> {
     public void setData(T data) {
         this.data = data;
     }
+    //Getter for self referential data
+    public Node<T> getNext() {
+        return next;
+    }
+
+    //Setter for self referential data
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
