283f71fb92d1ef785e71a93ecfdfd680d2f30384
shailja tripathi <shailja.tripathi_cs18@gla.ac.in>
2020-02-24 17:38:09 +0530

    methos to view choice

--- src/definition/Menu.java
+++ src/definition/Menu.java
@@ -1,10 +1,7 @@
 package definition;
 
-import definition.MyLinkedList;
-import Helper.Node;
 import Helper.Person;
-import java.util.ArrayList;
-import java.util.Collections;
+
 import java.util.Scanner;
 
 public class Menu {
@@ -15,3 +12,14 @@ public class Menu {
         scan = new Scanner(System.in);
         linkedList = new MyLinkedList<>();
     }
+    public int showChoice() {
+        System.out.println("Welcome to Devesh's Contact List App\n" +
+                "Press 1 to add a new contact\n" +
+                "Press 2 to view all contacts\n" +
+                "Press 3 to search for a contact\n" +
+                "Press 4 to delete a contact\n" +           //methohd to view choice
+                "Press 5 to exit program");
+        return scan.nextInt();
+    }
+
+}
