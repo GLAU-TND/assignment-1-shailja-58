e29472b67a0bcb557a788f4f833868f6fd47c04d
shailja tripathi <shailja.tripathi_cs18@gla.ac.in>
2020-02-24 19:28:13 +0530

    main class completed

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -8,6 +8,23 @@ public class Main {
             int n = menu.showChoice();
             switch(n) {
                 //creating the switch cases in main class
+                case 1:
+                    menu.addChoice();
+                    break;
+                case 2:
+                    menu.viewChoice();
+                    break;
+                case 3:
+                    menu.searchChoice();
+                    break;
+                case 4:
+                    menu.deleteChoice();
+                    break;
+                case 5:
+                    menu.exitChoice();
+                    flag = false;
+                    break;
+                    // main class completed
             }
         }
     }
