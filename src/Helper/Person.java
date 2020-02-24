8ab56a855751748fd8016dda02980f2fa71205af
shailja tripathi <shailja.tripathi_cs18@gla.ac.in>
2020-02-24 17:03:17 +0530

    getter for first name

--- src/Helper/Person.java
+++ src/Helper/Person.java
@@ -1,1 +1,14 @@
+package Helper;
 
+public abstract class Person implements Comparable<Person> {
+
+    private String firstName;                   //First name of person
+    private String lastName;                    //Last name of person
+    private String emailID;                     //EmailId of person
+    private StringBuffer phoneNumbers;          //Phone numbers of person
+
+    //Getter for first name
+    public String getFirstName() {
+        return firstName;
+    }
+}
