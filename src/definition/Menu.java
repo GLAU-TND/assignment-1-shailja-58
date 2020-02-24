515eb56120ae4cb2c0fa10fccb7312f35b48b9a3
shailja tripathi <shailja.tripathi_cs18@gla.ac.in>
2020-02-24 17:52:29 +0530

    searching the contact from first name

--- src/definition/Menu.java
+++ src/definition/Menu.java
@@ -93,4 +93,28 @@ public class Menu {
             System.out.println(node.getData());
         }
     }
+    public void searchChoice() {
+        int counter = 0;
+        System.out.print("You could search for a contact from their first names: ");
+        scan.nextLine();
+        String name = scan.nextLine().trim();
+
+        while(true){
+            Node<Person> node = linkedList.getObject();
+            if(node==null)
+                break;
+            if (node.getData().getFirstName().compareTo(name) == 0)
+                counter++;
+        }
+
+        System.out.println(counter + " match found!");
+        while(true){
+            Node<Person> node = linkedList.getObject();
+            if(node==null)
+                break;
+            if (node.getData().getFirstName().compareTo(name) == 0)
+                System.out.println(node.getData());
+        }
+    }
+    //searching the contact from first name
 }
